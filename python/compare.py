33f7d092131f95477ac81ca4692be4c47d4e819c
crosetto <crosetto@lifev.org>
2018-02-01 18:55:02 +0100

    updating python script

--- python/compare.py
+++ python/compare.py
@@ -4,11 +4,3 @@ def diff(string1, string2) :
     string2=str(string2).splitlines(1)
     diff=difflib.unified_diff(string1, string2)
     return ''.join(diff)
-
-# def continue_if_not(str3) :
-#     import gdb
-#     if str3 == "" :
-#         gdb.execute("continue")
-#     else :
-#         print str3
-#     return
